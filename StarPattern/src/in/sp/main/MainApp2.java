d5a14c556a533557737cbe9c7c61d61e4033edc2
Aashish240192 <aashish240192@gmail.com>
2025-04-10 21:26:46 +0530

    New Array  and Star Pattern

---
+++ StarPattern/src/in/sp/main/MainApp2.java
@@ -0,0 +1,22 @@
+package in.sp.main;
+
+public class MainApp2
+{
+    public static void main(String[] args)
+    {
+
+        for (int i = 1; i <= 5; i++)
+        {
+
+            for (int j = i ; j <= 5; j++)
+            {
+
+                System.out.print("*");
+
+            }
+            System.out.println();
+
+        }
+
+    }
+}
